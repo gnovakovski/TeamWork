3fda6e91fd730261954f7c18cbaba6000f97140d
luanmotta <luanmotta97@hotmail.com>
2017-09-03 17:18:43 -0300

    Add events feed components

--- src/views/EventsFeed.vue
+++ src/views/EventsFeed.vue
@@ -1,13 +1,38 @@
 <template>
-  <div class="events-feed">
-    Hi
+  <div id="events-feed">
+
+    <md-list class="md-triple-line">
+
+      <md-list-item v-for="event in events" :key="event._id">
+        <item-event :event="event"></item-event>
+        <md-divider></md-divider>
+      </md-list-item>
+
+    </md-list>
+
   </div>
 </template>
 
 <script>
-export default {
-  name: 'events-feed'
-}
+  // import axios from 'axios'
+
+  import ItemEvent from '@/components/Event/ItemEvent.vue'
+
+  export default {
+    components: {
+      ItemEvent
+    },
+    data: () => ({
+      events: []
+    }),
+    methods: {
+      fetchEvents () {
+      }
+    },
+    created () {
+      this.fetchEvents()
+    }
+  }
 </script>
 
 <style scoped>
