d2606f9b56bfab83364758d8ff46832e95da15ad
luanmotta <luanmotta97@hotmail.com>
2017-09-03 16:35:25 -0300

    Preserve hello route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import EventsFeed from '@/views/EventsFeed'
+import Hello from '@/views/Hello'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'EventsFeed',
       component: EventsFeed
+    },
+    {
+      path: '/Hello',
+      name: 'Hello',
+      component: Hello
     }
   ]
 })
