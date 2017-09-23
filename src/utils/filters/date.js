2a20d5235e552ad1debb0428bbb7b06e2ed7ef87
ezequielmross <ezequielmross@gmail.com>
2017-09-23 11:26:03 -0300

    fix date zeros

--- src/utils/filters/date.js
+++ src/utils/filters/date.js
@@ -1,6 +1,20 @@
+function datePlusZero(date) {
+  return date < 10 ? '0' + date : date
+}
+
 module.exports = {
+  datePlusZero(date) {
+    return date < 10 ? '0' + date : date
+  },
   'getDefaultFormat': date => {
     date = new Date(date)
-    return `${date.getDate()}/${date.getMonth()}/${date.getFullYear()} as ${date.getHours()}:${date.getMinutes()}`
+    date = {
+      day     : datePlusZero(date.getDate()),
+      month   : datePlusZero(date.getMonth()),
+      year    : date.getFullYear(),
+      hours   : datePlusZero(date.getHours()),
+      minutes : datePlusZero(date.getMinutes())
+    }
+    return `${date.day}/${date.month}/${date.year} às ${date.hours}:${date.minutes}`
   }
 }
