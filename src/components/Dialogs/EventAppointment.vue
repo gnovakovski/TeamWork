59076fdb8b4035c67566bb5dd8d43d8eeaec2b0a
ezequielmross <ezequielmross@gmail.com>
2017-10-07 15:26:10 -0300

    Done icons and eventAppointment

--- src/components/Dialogs/EventAppointment.vue
+++ src/components/Dialogs/EventAppointment.vue
@@ -8,7 +8,7 @@
       md-dialog-content
         form(novalidate @submit.stop.prevent="submit")
           ul
-            li(v-for="date in event.dates") {{ date | getDateWithoutTime }}
+            li(v-for="date in datesParsed") {{ date | getDateWithoutTime }}
               ul
                 li(v-for="time in date.times")
                   md-checkbox(v-model="time.isSelected") {{ time.value }}
@@ -31,23 +31,42 @@
     },
     filters: { getDateWithoutTime },
     props: ['from', 'event'],
-    data() {
-      return {}
+    computed: {
+      datesParsed() {
+        this.username = api.getUser().username
+
+        this.event.dates.map(d => {
+          d.times.map(t => {
+            t.isSelected = (t.people || []).indexOf(this.username) !== -1
+          })
+        })
+
+        return this.event.dates
+      }
     },
     methods: {
       saveAppoinment() {
+        let i
+
         this.username = api.getUser().username
+
         this.event.dates.map(d => {
           d.times.map(t => {
-            if (t.isSelected) {
-              delete t.isSelected
+            t.people = t.people || []
+            i = t.people.indexOf(this.username)
 
-              t.people = t.people || []
-              if (t.people.indexOf(this.username) === -1)
+            if (t.isSelected) {
+              if (i === -1)
                 t.people.push(this.username)
+            } else {
+              if (i > -1)
+                t.people.splice(i, 1)
             }
+
+            delete t.isSelected
           })
         })
+
         api.events.child(this.event['key']).set(this.event)
           .then(() => this.$refs['snackbar'].show('Confirmado'))
           .catch(() => this.$refs['snackbar'].show('Desculpe, tente novamente mais tarde'))
